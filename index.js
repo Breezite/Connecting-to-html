580166dfa28610e3238de495404224a01c5dd2d3
Julian M <mifsudjulian007@gmail.com>
2025-02-23 23:48:20 +0100

    LittleMore

--- index.js
+++ index.js
@@ -8,3 +8,46 @@ obj[0] = 4;
 obj[1] = 'red';
 console.log(obj);
 console.log('Test Commit');
+
+let currentScore = 101;
+let highScore = 100;
+highScore = currentScore > highScore ? currentScore : highScore;
+console.log(highScore);
+
+const output = fizzBuzz('dsa');
+console.log(output);
+
+function fizzBuzz(input){
+    if((input % 3 == 0) && (input % 5 == 0))
+    {
+        return 'FizzBuzz';
+    }
+    if(input % 5 == 0)
+    {
+        return 'Buzz';
+    }
+    if(input % 3 == 0)
+    {
+        return 'Fizz';
+    }
+    if(typeof(input) !== 'number')
+    {
+        return 'Not a number';
+    }
+}
+
+const array = [0, 1, '', 3, undefined, 4];
+console.log(countTruths(array));
+
+function countTruths(array)
+{
+    let counter = 0;
+    for(let i = 0; i < array.length; i++)
+    {
+        if(array[i])
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
